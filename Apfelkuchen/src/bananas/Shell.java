c675e49e5bb282bbed9c0ab425e6084e8f6c2c9e
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2014-12-17 23:28:25 +0100

    Let the monkey fly!!

--- Apfelkuchen/src/bananas/Shell.java
+++ Apfelkuchen/src/bananas/Shell.java
@@ -1,12 +1,22 @@
 package bananas;
 
+import java.util.LinkedList;
+
+import bananas.parser.Parser;
+import bananas.parser.Tag;
+
 public class Shell {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        Connector paulSmith = new Connector("http://www.google.com");
-        paulSmith.start();
-
-    }
+      Connector paulSmith = new Connector("http://google.com/");
+      paulSmith.start();
+      System.out.println(paulSmith.getContent());
+      Parser p2 = new Parser(paulSmith.getContent());
+      LinkedList<Tag> l = p2.parseLinks();
+      for (Tag tag : l) {
+         System.out.println("LINK >" + tag.getAttr());
+      }
+   }
 
 }
---
+++ Apfelkuchen/src/bananas/parser/Parser.java
@@ -0,0 +1,162 @@
+package bananas.parser;
+
+import java.util.LinkedList;
+
+public class Parser {
+   private String html;
+   private String tag = "", attr, content;
+
+   public static final int STATE_IN_TAG_CONTENT = 0;
+   public static final int STATE_IN_ATTR = 1;
+   public static final int STATE_IN_TAG_END = 2;
+   public static final int STATE_IN_TAG_START = 3;
+   public static final int STATE_FAULTY_PART = 4;
+
+   public static final String[] wontclose = {"br", "meta", "hr", "img", "wbr",
+         "area", "img", "input", "option", "link", "source" };
+
+   public Parser(String html) {
+      this.html = html;
+      attrReset();
+   }
+
+   public static int seqAppeares(String part, String content) {
+      int i = 0;
+      int lindex = -1;
+      do {
+         lindex = content.indexOf(part, lindex + 1);
+         i++;
+      } while (lindex != -1);
+      return i;
+   }
+
+   public LinkedList<Tag> parseString(String txt) throws Exception {
+      LinkedList<Tag> tags = new LinkedList<Tag>();
+      int state = -1;
+      int laststable = 0;
+      for (int i = 0; i < txt.length(); i++) {
+         char c = txt.charAt(i);
+
+         switch (state) {
+         case STATE_FAULTY_PART:
+            if (c == '>') {
+               state = -1;
+            } else {
+
+            }
+            break;
+         case STATE_IN_TAG_START:
+            if (c == ' ') {
+               state = STATE_IN_ATTR;
+            } else if (c == '!' && txt.charAt(i - 1) == '<') {
+               state = STATE_FAULTY_PART;
+            } else if (c == '>') {
+               if (wontClose(tag)) {
+                  pushTag(tags);
+                  laststable = i;
+                  state = -1;
+               } else
+                  state = STATE_IN_TAG_CONTENT;
+            } else if (c == '/') {
+               state = STATE_FAULTY_PART;
+            } else {
+               tag += c;
+            }
+            break;
+         case STATE_IN_ATTR:
+            if (c == '>') {
+               if (wontClose(tag)) {
+                  pushTag(tags);
+                  laststable = i;
+                  state = -1;
+               } else
+                  state = STATE_IN_TAG_CONTENT;
+            } else {
+               attr += c;
+            }
+            break;
+         case STATE_IN_TAG_CONTENT:
+            String searched = "</" + tag + ">";
+            String start = "<" + tag;
+            if (i + searched.length() > txt.length()) {
+               System.err.println("We will run out of the string!!");
+               System.err.println("At tag {" + tag + "} part: {..."
+                     + txt.substring(laststable) + "}");
+               System.err.println("We better return, FUCK THIS SITE!");
+               content += txt.substring(i);
+               pushTag(tags);
+               return tags;
+            }
+            String got = txt.substring(i, i + searched.length());
+            if (got.equals(searched)
+                  && seqAppeares(start, content) == seqAppeares(searched,
+                        content)) {
+               pushTag(tags);
+               state = -1;
+               laststable = i;
+               i += searched.length() - 1;
+            } else {
+               content += c;
+            }
+            break;
+         default:
+            if (c == '<') {
+               state = STATE_IN_TAG_START;
+            } else {
+
+            }
+            break;
+         }
+      }
+      return tags;
+   }
+
+   public LinkedList<Tag> parse(String txt) throws Exception {
+
+      LinkedList<Tag> result = new LinkedList<Tag>();
+      LinkedList<Tag> res = parseString(txt);
+      if ((res == null) || res.isEmpty())
+         return null;
+      for (Tag tag : res) {
+         LinkedList<Tag> r = parse(tag.getContent());
+         result.add(tag);
+         if (r != null)
+            result.addAll(r);
+      }
+      return result;
+   }
+
+   public LinkedList<Tag> parseLinks() {
+      LinkedList<Tag> tags = null;
+      LinkedList<Tag> result = new LinkedList<Tag>();
+      try {
+         tags = parse(html);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      for (Tag tag : tags) {
+         if (tag.getName().equals("a"))
+            result.add(tag);
+      }
+      return tags;
+   }
+
+   private static boolean wontClose(String tag) {
+      for (String string : wontclose) {
+         if (string.equals(tag))
+            return true;
+      }
+      return false;
+   }
+
+   private void attrReset() {
+      tag = "";
+      attr = "";
+      content = "";
+   }
+
+   private void pushTag(LinkedList<Tag> tags) {
+      tags.add(new Tag(tag, attr, content));
+      attrReset();
+   }
+}
