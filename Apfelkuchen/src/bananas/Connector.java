4d74df3b73fb39688e239b6bb3053291d6ef2e8d
hectorPascal <sly94@live.de>
2014-12-17 18:58:23 +0100

    Initial version of Apfelkuchen

---
+++ Apfelkuchen/src/bananas/Connector.java
@@ -0,0 +1,60 @@
+package bananas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+
+public class Connector {
+    private URL url;
+
+    public String content = "";
+
+    public Connector(String url) {
+        try {
+            this.url = new URL(url);
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void start() {
+        URLConnection connection;
+        InputStream in = null;
+        try {
+
+            connection = url.openConnection();
+            in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(in));
+            while (reader.ready()) {
+                content += reader.readLine();
+
+            }
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                in.close();
+            } catch (IOException e2) {
+                System.out.println("Bananananas!");
+
+            }
+        }
+
+    }
+}
---
+++ Apfelkuchen/src/bananas/Shell.java
@@ -0,0 +1,12 @@
+package bananas;
+
+public class Shell {
+
+    public static void main(String[] args) {
+
+        Connector paulSmith = new Connector("http://www.google.com");
+        paulSmith.start();
+
+    }
+
+}
