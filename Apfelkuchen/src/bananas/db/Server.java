a2ef08813924aef2ceaf78dc92b2ce99d4235a20
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2014-12-18 19:01:12 +0100

    Added Database skeleton

---
+++ Apfelkuchen/src/bananas/db/Server.java
@@ -0,0 +1,19 @@
+package bananas.db;
+
+
+public class Server {
+
+   public Database b;
+
+   public Server(Database b) {
+      this.b = b;
+   }
+
+   public ServerFile getFile(String file) {
+      return null;
+   }
+
+   public void addFile(String file) {
+
+   }
+}
---
+++ Apfelkuchen/src/bananas/db/ServerFile.java
@@ -0,0 +1,15 @@
+package bananas.db;
+
+
+public class ServerFile {
+   private Database b;
+
+   public ServerFile(Database b) {
+      this.b = b;
+
+   }
+
+   public void addCall(String params) {
+
+   }
+}
